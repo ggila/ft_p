c74ebd76487ae09524d77a7b206a44631f9001dd
Gautier GILABERT <ggilaber@e1r5p23.42.fr>
2015-10-09 18:51:17 +0200

    MakefileS

--- src/serveur/serverloop.c
+++ src/serveur/serverloop.c
@@ -6,7 +6,7 @@
 /*   By: ggilaber <ggilaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/08 22:07:17 by ggilaber          #+#    #+#             */
-/*   Updated: 2015/10/08 22:52:08 by ggilaber         ###   ########.fr       */
+/*   Updated: 2015/10/09 17:03:02 by ggilaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,22 @@ void	serverloop(int sock)
 	ft_putstr(buf);
 	close(client_sock);
 }
+
+
+/*
+void	serverloop(int sock)
+{
+	fd_set 	fds;
+
+	while (1)
+	{
+		FD_ZERO(&fds);
+		add_client_socket();
+		FD_SET(sock, &fds);
+		if (select(max + 1, &fdset, NULL, NULL, NULL) == pasbon)
+			return
+		if (FD_ISSET(sock, fdset))
+			acceptclient();
+		check_fdset()  // pour chaque client, verifie si il parle ()
+	}
+}*/
